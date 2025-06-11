80d5f3c736134dca1d70b7076ab982dcd1489f9c
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2025-06-11 08:51:09 -0700

    feature/11 | Create Entity and Entity Graph

---
+++ api/src/main/java/com/smartcart/api/model/entity/Product.java
@@ -0,0 +1,40 @@
+package com.smartcart.api.model.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Float price;
+
+    private String imageUrl;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+}
