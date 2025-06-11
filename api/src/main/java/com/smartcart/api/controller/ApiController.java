d1be48763021b12c02686b9df1cf3c860b11f71b
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2025-06-11 08:52:42 -0700

    feature/11 | Created endpoint, getAllCategoryProducts

--- api/src/main/java/com/smartcart/api/controller/ApiController.java
+++ api/src/main/java/com/smartcart/api/controller/ApiController.java
@@ -1,17 +1,31 @@
 package com.smartcart.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.smartcart.api.model.dto.CategoryDTO;
+import com.smartcart.api.service.CategoryService;
 
 // https://www.baeldung.com/spring-rest-openapi-documentation
 @RestController
 @RequestMapping("/api")
 public class ApiController {
+
+    @Autowired
+    private CategoryService categoryService;
     
     @GetMapping("/hello")
     public String helloWorld() {
         return "Hello, World!";
+    }
+
+    @GetMapping("/category/products")
+    public List<CategoryDTO> getAllCategoryProducts() {
+        return categoryService.getAllCategoryProducts();
     }
     
 }
