673c8c74577d63e8201807776728fb933385a6a0
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2025-06-11 22:12:23 -0700

    feature/14 | Add QueryClient

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,11 +1,15 @@
-import { Navbar } from './components';
+import { Navbar, DropDown } from './components';
 import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => {
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
 			<Navbar />
-		</>
+			<DropDown />
+		</QueryClientProvider>
 	);
 };
 
