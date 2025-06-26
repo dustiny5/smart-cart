54e94ef72f411a37f6377d7675f2c3b448f5bbde
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2025-06-25 18:49:56 -0700

    feature/14 | Create re-usable hook to hide dropdown

---
+++ webapp/src/components/Header/useHidden.ts
@@ -0,0 +1,24 @@
+import { useEffect, type RefObject } from 'react';
+
+export const useHidden = (
+	refParent: RefObject<HTMLElement | null>,
+	refChild: RefObject<HTMLElement | null>,
+	callback: () => void
+) => {
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			// Must create another ref(refChild) to prevent rendering 2 times.
+			if (
+				!refParent.current?.contains(event.target as HTMLElement) &&
+				!refChild.current?.contains(event.target as HTMLElement)
+			) {
+				callback();
+			}
+		};
+		document.addEventListener('mousedown', handleClickOutside);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, []);
+};
