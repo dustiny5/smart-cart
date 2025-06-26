8ce89ce9fa6f642b9ad7d6decbffbf1597900f01
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2025-06-26 09:46:12 -0700

    feature/14 | Create DropDown component

--- webapp/src/components/Header/DropDown.tsx
+++ webapp/src/components/Header/DropDown.tsx
@@ -1,40 +1,57 @@
 import './DropDown.css';
 import { useQuery } from '@tanstack/react-query';
+import { useHidden } from './useHidden';
+import type { RefObject } from 'react';
 
-const DropDown = () => {
-	//https://tanstack.com/query/latest/docs/framework/react/examples/simple
+type DropDownProps = {
+	onRefDropDown: RefObject<HTMLDivElement | null>;
+	onRefBtn: RefObject<HTMLButtonElement | null>;
+	onSetIsHidden: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
-	// const { isPending, data, isFetching, error } = useQuery({
-	// 	queryKey: ['dropdownData'],
-	// 	queryFn: async () => {
-	// 		const response = await fetch(
-	// 			import.meta.env.SMART_CART_API_URL + '/category/products'
-	// 		);
-	// 		return await response.json();
-	// 	},
-	// });
-	// if (isPending) {
-	// 	return <div>Loading...</div>;
-	// }
-	// if (error) {
-	// 	return <div>{`An error has occured` + error.message}</div>;
-	// }
+const DropDown = ({
+	onRefDropDown,
+	onRefBtn,
+	onSetIsHidden,
+}: DropDownProps) => {
+	useHidden(onRefDropDown, onRefBtn, () => onSetIsHidden(true));
+	// https://tanstack.com/query/latest/docs/framework/react/examples/simple
+	const { isPending, isSuccess, data, error } = useQuery({
+		queryKey: ['dropdownData'],
+		queryFn: async () => {
+			const response = await fetch(
+				import.meta.env.VITE_SMART_CART_API_URL + '/category/products'
+			);
+			return await response.json();
+		},
+		staleTime: Infinity,
+	});
 
 	return (
-		<div className="dropdown">
-			<button className="dropdown-btn">Dropdown</button>
+		<div className="dropdown" ref={onRefDropDown}>
+			{isPending && <div>Loading...</div>}
+			{error && <div>An error has occured. Please try again...</div>}
 			<div className="dropdown-container">
-				<div className="dropdown-content">
-					<h4>Category</h4>
-					<button className="dropdown-link">Link 1</button>
-					<button className="dropdown-link">Link 2</button>
-					<button className="dropdown-link">Link 3</button>
-				</div>
+				{isSuccess &&
+					data.map((item: any) => (
+						<div key={item.id} className="dropdown-content">
+							<h4>{item.name}</h4>
+							{item.products.map((product: any) => (
+								<button
+									key={product.id}
+									className="dropdown-item"
+								>
+									{product.name}
+								</button>
+							))}
+						</div>
+					))}
+				{/* Delete and add more categories later... */}
 				<div className="dropdown-content">
 					<h4>Hats</h4>
-					<button className="dropdown-link">Link 4</button>
-					<button className="dropdown-link">Link 5</button>
-					<button className="dropdown-link">Link 6</button>
+					<button className="dropdown-item">Link 4</button>
+					<button className="dropdown-item">Link 5</button>
+					<button className="dropdown-item">Link 6</button>
 				</div>
 			</div>
 		</div>
