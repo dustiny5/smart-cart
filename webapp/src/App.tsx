54acaeb58a08d0e94e6bb05fa49f3ea59427845e
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2025-07-08 17:49:30 -0700

    feature/25 | Reset DOM to the Home Page

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,15 +1,21 @@
-import { BestSeller, Carousel, Navbar } from './components';
+import { Body, Navbar } from './components';
 import './App.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 
 const queryClient = new QueryClient();
 
 const App = () => {
+	const [resetToggle, setResetToggle] = useState(false);
+	const handleHomeClick = () => setResetToggle(false);
+
 	return (
 		<QueryClientProvider client={queryClient}>
-			<Navbar />
-			<Carousel />
-			<BestSeller />
+			<Navbar onHandleHomeClick={handleHomeClick} />
+			<Body
+				onResetToggle={resetToggle}
+				onSetResetToggle={setResetToggle}
+			/>
 		</QueryClientProvider>
 	);
 };
