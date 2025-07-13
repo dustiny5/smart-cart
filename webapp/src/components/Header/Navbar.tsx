f5d3a1cc95059a258b0ceb03cb7af29d1abe82df
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2025-07-12 22:25:48 -0700

    feature/35 | Create ShoppingCart

--- webapp/src/components/Header/Navbar.tsx
+++ webapp/src/components/Header/Navbar.tsx
@@ -1,5 +1,6 @@
 import './Navbar.css';
 import Shop from './Shop';
+import ShoppingCart from './ShoppingCart';
 
 type NavbarProps = {
 	onHandleHomeClick: () => void;
@@ -53,22 +54,7 @@ const Navbar = ({ onHandleHomeClick }: NavbarProps) => {
 								/>
 							</svg>
 						</button>
-						<button>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								strokeWidth={1.5}
-								stroke="currentColor"
-								className="size-6"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-								/>
-							</svg>
-						</button>
+						<ShoppingCart />
 						<button>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
