b946c2585fbfd0f6b82a394b7f526b547477460f
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2025-07-25 19:01:16 -0700

    feature/49 | Remove mapstruct warning add ignore

--- api/src/main/java/com/smartcart/api/model/mapper/ProductMapper.java
+++ api/src/main/java/com/smartcart/api/model/mapper/ProductMapper.java
@@ -3,6 +3,7 @@ package com.smartcart.api.model.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 
@@ -11,7 +12,13 @@ import com.smartcart.api.model.entity.Product;
 
 @Mapper(componentModel = "spring", uses = CategoryMapper.class)
 public interface ProductMapper {
+    
     ProductDTO toDTO(Product product);
+
+    @Mapping(target="createdAt", ignore=true)
+    @Mapping(target="updatedAt", ignore=true)
+    @Mapping(target="isBestSeller", ignore=true)
+    @Mapping(target="category", ignore=true)
     Product toEntity(ProductDTO productDTO);
 
     default Page<ProductDTO> toDTOPage(Page<Product> productPage) {
