5767af29b7e4e34b107d4412e72194a07f6717cb
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2025-08-01 16:18:53 -0700

    feature/47 | Create endpoint to search by similar product names

--- api/src/main/java/com/smartcart/api/controller/SmartCartController.java
+++ api/src/main/java/com/smartcart/api/controller/SmartCartController.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.smartcart.api.model.dto.CategoryDTO;
 import com.smartcart.api.model.dto.ProductDTO;
+import com.smartcart.api.model.entity.Product;
 import com.smartcart.api.service.CategoryService;
 import com.smartcart.api.service.ProductService;
 
@@ -21,6 +23,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.constraints.NotBlank;
 
 // https://www.baeldung.com/spring-rest-openapi-documentation
 @OpenAPIDefinition(
@@ -68,6 +71,21 @@ public class SmartCartController {
     @GetMapping("/products/bestSeller")
     public Page<ProductDTO> getProductsIsBestSeller(@ParameterObject Pageable pageable) {
         return productService.getProductsIsBestSeller(pageable);
+    }
+
+    @Operation(summary = "Search for similar named products.",
+            description = "Search for similar named products.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Find similar named products",
+                content = {
+                    @Content(mediaType = "application/json")}),
+        @ApiResponse(responseCode = "404", description = "Products not found",
+                content = @Content),
+        @ApiResponse(responseCode = "500", description = "Server error",
+                content = @Content)})
+    @GetMapping("/products/similar/{name}")
+    public List<ProductDTO> getProductsSimilarName(@PathVariable @NotBlank String name) {
+        return productService.getProductsSimilarName(name);
     }
 
 }
--- api/src/main/java/com/smartcart/api/repository/ProductRepository.java
+++ api/src/main/java/com/smartcart/api/repository/ProductRepository.java
@@ -1,14 +1,20 @@
 package com.smartcart.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.smartcart.api.model.entity.Product;
 
 @Repository
-public interface ProductRepository extends PagingAndSortingRepository<Product, Long>{
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
 
     Page<Product> findByIsBestSellerTrue(Pageable pageable);
+
+    @Query(value = "SELECT * FROM smart_cart.product WHERE name % ?1", nativeQuery = true)
+    List<Product> findBySimilarName(String name);
 }
