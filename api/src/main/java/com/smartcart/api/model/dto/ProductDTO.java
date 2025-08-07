691880dfae4c1fe50c0dc0b958fb92372911e50e
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2025-08-06 20:50:01 -0700

    feature/51 | Add quantity to dto

--- api/src/main/java/com/smartcart/api/model/dto/ProductDTO.java
+++ api/src/main/java/com/smartcart/api/model/dto/ProductDTO.java
@@ -6,10 +6,12 @@ import lombok.Data;
 
 @Data
 public class ProductDTO {
+
     private Long id;
     private String name;
     private String description;
     private Float price;
     private List<String> tags;
     private String imageUrl;
+    private Integer quantity;
 }
