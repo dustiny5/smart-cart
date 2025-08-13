9b21fb0754a2aa7e05330d920d301310194081c7
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2025-08-12 19:40:11 -0700

    feature/102 | Return cookie to response headers

--- api/src/main/java/com/smartcart/api/service/AuthServiceImpl.java
+++ api/src/main/java/com/smartcart/api/service/AuthServiceImpl.java
@@ -1,22 +1,29 @@
 package com.smartcart.api.service;
 
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import com.nimbusds.jwt.SignedJWT;
+import com.smartcart.api.exception.InternalServerException;
 import com.smartcart.api.model.dto.CognitoJwtResponse;
-import com.smartcart.api.model.dto.CognitoUserResponse;
+import com.smartcart.api.model.dto.CognitoUserInfoResponse;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Service
 public class AuthServiceImpl implements AuthService {
@@ -33,13 +40,20 @@ public class AuthServiceImpl implements AuthService {
     @Value("${aws.cognito.redirect-uri}")
     private String redirectUri;
 
+    @Value("${cookie.secure}")
+    private Boolean cookieSecure;
+
+    @Value("${cookie.same-site}")
+    private String cookieSameSite;
+
+    @Autowired
+    private JwtDecoder jwtDecoder;
+
     @Override
-    public CognitoUserResponse getCurrentUser(JwtAuthenticationToken token) {
-        Jwt jwt = token.getToken();
-        CognitoUserResponse cognitoUser = new CognitoUserResponse();
-        cognitoUser.setUsername(jwt.getClaimAsString("cognito:username"));
-        cognitoUser.setEmail(jwt.getClaimAsString("email"));
-        cognitoUser.setGroups(jwt.getClaimAsString("cognito:groups"));
+    public CognitoUserInfoResponse getCurrentUser(String jwt) {
+        Jwt jwtDecoded = jwtDecoder.decode(jwt);
+        CognitoUserInfoResponse cognitoUser = new CognitoUserInfoResponse();
+        cognitoUser.setGivenName(jwtDecoded.getClaimAsString("given_name"));
         return cognitoUser;
     }
 
@@ -47,7 +61,7 @@ public class AuthServiceImpl implements AuthService {
      * Code can ONLY be exchanged once.
      */
     @Override
-    public CognitoJwtResponse exchangeCodeForToken(String code) {
+    public ResponseEntity<CognitoUserInfoResponse> exchangeCodeForToken(String code, HttpServletResponse response) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
@@ -61,10 +75,34 @@ public class AuthServiceImpl implements AuthService {
         form.add("redirect_uri", redirectUri);
         form.add("client_id", clientId);
 
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(form, headers);
+        HttpEntity<MultiValueMap<String, String>> jwtRequest = new HttpEntity<>(form, headers);
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<CognitoJwtResponse> response = restTemplate.postForEntity(tokenUri, request, CognitoJwtResponse.class);
-        return response.getBody();
+        ResponseEntity<CognitoJwtResponse> jwtResponse = restTemplate.postForEntity(tokenUri, jwtRequest, CognitoJwtResponse.class);
+
+        if (jwtResponse.getStatusCode().is2xxSuccessful() && jwtResponse.getBody() != null) {
+            String idToken = jwtResponse.getBody().getId_token();
+
+            try {
+                SignedJWT signedJWT = SignedJWT.parse(idToken);
+                String givenName = signedJWT.getJWTClaimsSet().getStringClaim("given_name");
+                Long expiresIn = (signedJWT.getJWTClaimsSet().getExpirationTime().getTime() - signedJWT.getJWTClaimsSet().getIssueTime().getTime()) / 1000;
+                ResponseCookie jwtCookie = ResponseCookie.from("jwt", idToken)
+                        .httpOnly(true)
+                        .secure(cookieSecure)
+                        .path("/")
+                        .maxAge(expiresIn)
+                        .sameSite(cookieSameSite)
+                        .build();
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                        .body(new CognitoUserInfoResponse(givenName));
+
+            } catch (ParseException ex) {
+                throw new InternalServerException("Failed to parse JWT token");
+            }
+
+        }
+        throw new InternalServerException("Failed to exchange code for token or token is null");
     }
 }
