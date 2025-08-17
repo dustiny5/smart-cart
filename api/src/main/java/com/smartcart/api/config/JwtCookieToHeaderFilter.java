58253a1b0457c3038df3379fd2f76e965603708e
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2025-08-17 09:38:02 -0700

    BE: Fix Warnings

--- api/src/main/java/com/smartcart/api/config/JwtCookieToHeaderFilter.java
+++ api/src/main/java/com/smartcart/api/config/JwtCookieToHeaderFilter.java
@@ -7,6 +7,7 @@ import java.util.Enumeration;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.lang.NonNull;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -19,9 +20,9 @@ import jakarta.servlet.http.HttpServletResponse;
 public class JwtCookieToHeaderFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
         String jwtToken = null;
